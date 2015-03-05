eca9a3c9e047b2d2e52a24577c61ee2c1c27a653
Hack Reactor Students <accounts@hackreactor.com>
2015-03-04 20:06:47 -0800

    Tweaking our grunt tasks
    -grunting and twerking

--- Gruntfile.js
+++ Gruntfile.js
@@ -117,6 +117,7 @@ module.exports = function(grunt) {
 
   grunt.registerTask('upload', function(n) {
     if(grunt.option('prod')) {
+        // grunt.task.run(['jshint', 'test', 'concat', 'uglify', 'cssmin'])
 
 
       // add your production server task here
