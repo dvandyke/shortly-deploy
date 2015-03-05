cb439e1dc268cde005cdb552921ec9fe7fbabf80
Hack Reactor Students <accounts@hackreactor.com>
2015-03-05 11:25:08 -0800

    Start transition from SQLite to MongoDB/Mongoose

--- app/models/user.js
+++ app/models/user.js
@@ -1,7 +1,9 @@
 var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
+var mongoose = require('mongoose');
 
+var User = mongoose.model('User', userSchema);
 var User = db.Model.extend({
   tableName: 'users',
   hasTimestamps: true,
